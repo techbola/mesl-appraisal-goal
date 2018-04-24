bbb5af60fd43b1bbbb2ce5028d79d9674aca4a9b
Stanley Umeanozie <coolstanlee@gmail.com>
2018-04-24 16:38:20 +0100

    Fixed dropdowns not working in modals.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -74,7 +74,7 @@
 
   {{-- MODALS --}}
   <!-- Modal -->
-  <div class="modal fade slide-up disable-scroll" id="new_project" tabindex="-1" role="dialog" aria-hidden="false">
+  <div class="modal fade slide-up disable-scroll" id="new_project" role="dialog" aria-hidden="false">
     <div class="modal-dialog ">
       <div class="modal-content-wrapper">
         <div class="modal-content">
--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -44,7 +44,7 @@
 
 	{{-- MODALS --}}
 	<!-- Modal -->
-  <div class="modal fade slide-up disable-scroll" id="new_staff" tabindex="-1" role="dialog" aria-hidden="false">
+  <div class="modal fade slide-up disable-scroll" id="new_staff" role="dialog" aria-hidden="false">
     <div class="modal-dialog ">
       <div class="modal-content-wrapper">
         <div class="modal-content">
