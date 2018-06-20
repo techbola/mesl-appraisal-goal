e57b55298793d19dacf513385ec85799f716473b
Stanley Umeanozie <coolstanlee@gmail.com>
2018-06-20 17:28:01 +0100

    Embed user guide in help section.

---
+++ resources/views/help.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="card-box">
+    <div class="card-title">User Guide</div>
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <iframe src = "{{ asset('/assets/plugins/ViewerJS/#/docs/documentation.pdf') }}" width="100%" height="700" allowfullscreen webkitallowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@push('scripts')
+  <script>
+    $('#spinner').show();
+    setTimeout(function(){
+      $('#spinner').hide();
+    }, 7000);
+  </script>
+@endpush
