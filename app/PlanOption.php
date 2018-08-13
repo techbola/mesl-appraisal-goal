bc38702428f7074ba80db757ccfd4a548ade6d19
Shola Fayiga <sola.fayiga@cavidel.com>
2018-08-13 20:10:38 +0100

    Add Plan option to bill

---
+++ app/PlanOption.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cavidel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlanOption extends Model
+{
+    protected $table   = 'tblPlanOption';
+    protected $guarded = ['OptionRef'];
+    public $primaryKey = 'OptionRef';
+    public $timestamps = false;
+}
