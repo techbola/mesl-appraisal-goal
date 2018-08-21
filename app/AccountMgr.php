822c8bcd0d2ab9b4bfde897fcdcd93e2179ff352
Dipo Odeyemi <dipo.odeyemi@cavidel.com>
2018-08-21 15:00:14 +0100

    Update account mgr model

--- app/AccountMgr.php
+++ app/AccountMgr.php
@@ -6,15 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class AccountMgr extends Model
 {
-<<<<<<< HEAD
+
     protected $table   = 'tblAccountMgr';
     protected $guarded = ['AccountMgrRef'];
     public $primaryKey = 'AccountMgrRef';
     public $timestamps = false;
-=======
-    protected $table      = 'tblAccountMgr';
-    protected $guarded    = ['AccountMgrRef'];
-    protected $primaryKey = 'AccountMgrRef';
-    public $timestamps    = false;
-}
->>>>>>> 7e3037caca08a008c0cb8a8d867ff593a0a779f2
+
