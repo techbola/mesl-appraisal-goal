2fa905fed188f1eef25642a5ac4368c38ba37b69
Riliwan Balogun <riliwan.rabo@gmail.com>
2018-08-23 12:48:46 +0100

    Receipts

--- resources/views/receipts/template_pdf.blade.php
+++ resources/views/receipts/template_pdf.blade.php
@@ -10,6 +10,7 @@
       @endif
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+     <meta charset="utf-8">
 
      <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
