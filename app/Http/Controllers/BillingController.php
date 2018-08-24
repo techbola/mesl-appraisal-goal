f99c094e17ca0747ff986996a2f433037b7db706
Riliwan Balogun <riliwan.rabo@gmail.com>
2018-08-24 08:12:24 +0100

    receipt dom pdf

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -418,7 +418,7 @@ class BillingController extends Controller
         $narrations      = $cash_entry->bill_narrations;
         $aw              = new NumberFormatter("en-GB", NumberFormatter::SPELLOUT);
         $amount_in_words = $aw->format($cash_entry->Amount);
-        PDF::setOptions(['dpi' => 200, 'defaultPaperSize' => "letter", 'defaultFont' => 'sans-serif']);
+        PDF::setOptions(['dpi' => 96, 'defaultPaperSize' => "letter", 'defaultFont' => 'sans-serif']);
         $pdf = PDF::loadView('receipts.template_pdf', compact('company_details', 'narrations', 'client_details', 'cash_entry', 'amount_in_words'));
         return $pdf->download('receipt.pdf');
         // $pdf = Cavidel::make('dompdf.wrapper');
