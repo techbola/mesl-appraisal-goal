0cf77e92035993ac3198ac01b6344675fa1e4d82
Riliwan Balogun <riliwan.rabo@gmail.com>
2018-08-24 08:38:07 +0100

    receipt dom pdf

--- resources/views/receipts/template_pdf.blade.php
+++ resources/views/receipts/template_pdf.blade.php
@@ -2,13 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>
-      @if(View::hasSection('title'))
-        OfficeMate - @yield('title')
-      @else
-          OfficeMate
-      @endif
-    </title>
+   
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
      <meta charset="utf-8">
      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
