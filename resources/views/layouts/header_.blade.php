259541fa02934be828c75c895f636c801acd6c4c
Stanley Umeanozie <coolstanlee@gmail.com>
2018-11-27 12:48:00 +0100

    Minor changes

--- resources/views/layouts/header_.blade.php
+++ resources/views/layouts/header_.blade.php
@@ -358,7 +358,7 @@
           {{-- @include('layouts.partials.topmenu') --}}
 
           <!-- START JUMBOTRON -->
-          <div class="jumbotron" data-pages="parallax">
+          <div class="jumbotron m-t-50" data-pages="parallax">
             <div class="container-fluid container-fixed-lg sm-p-l-20 sm-p-r-20">
               <div class="inner">
                 <!-- START BREADCRUMB -->
