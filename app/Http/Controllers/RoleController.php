575f2bc79024bf361213189687a171ad13a58f98
Riliwan Balogun <riliwan.rabo@gmail.com>
2019-01-08 17:03:03 +0100

    roles edit issue

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,40 +14,39 @@ class RoleController extends Controller
     }
     public function index()
     {
-        $user = auth()->user();
+        $user  = auth()->user();
         $roles = Role::where('CompanyID', $user->staff->CompanyID)->get();
         return view('roles.index', compact('roles'));
     }
     public function create()
     {
-      $user = auth()->user();
-        $roles = Role::where('CompanyID', $user->staff->CompanyID)->get();
+        $user      = auth()->user();
+        $roles     = Role::where('CompanyID', $user->staff->CompanyID)->get();
         $companies = Company::all();
         return view('roles.create', compact('roles', 'companies'));
     }
     public function store(Request $request)
     {
-      $this->validate($request, [
-          'name' => 'required|unique:roles|max:255',
-      ]);
+        $this->validate($request, [
+            'name' => 'required|unique:roles|max:255',
+        ]);
 
-        $role = new Role;
-        $role->name = strtolower($request->name);
+        $role               = new Role;
+        $role->name         = strtolower($request->name);
         $role->display_name = $request->display_name;
         $role->description  = $request->description;
         if (auth()->user()->is_superadmin) {
-          $role->CompanyID = $request->CompanyID;
+            $role->CompanyID = $request->CompanyID;
         } else {
-          $role->CompanyID = auth()->user()->staff->company->CompanyRef;
+            $role->CompanyID = auth()->user()->staff->company->CompanyRef;
         }
         // $menus = Menu::whereIn('slug', ['projects'])->get();
 
         // Let this role have same menus as basic "staff"
 
-
         if ($role->save()) {
             $menus = Role::where('name', 'staff')->first()->menus;
-            $role->menus()->attach( $menus->pluck('id')->toArray() );
+            $role->menus()->attach($menus->pluck('id')->toArray());
 
             return redirect()->back()->with('success', $role->name . ' has been added to roles successfully.');
         } else {
@@ -61,8 +60,9 @@ class RoleController extends Controller
     }
     public function edit($id)
     {
-        $role = Role::find($id);
-        return view('roles.edit', compact('role'));
+        $role      = Role::find($id);
+        $companies = Company::all();
+        return view('roles.edit', compact('role', 'companies'));
     }
     public function update(Request $request, $id)
     {
