a0d0e890190945e8c2c93ee822768a973ca2337e
Riliwan Balogun <riliwan.rabo@gmail.com>
2019-01-17 16:19:30 +0100

    travel request

--- app/Http/Controllers/TravelRequestController.php
+++ app/Http/Controllers/TravelRequestController.php
@@ -18,11 +18,11 @@ use MESL\Mail\RequestedRejected;
 use Mail;
 
 class TravelRequestController extends Controller
-{   
+{
 
     //Get data in the travel request table
-    public function create() {
-
+    public function create()
+    {
 
         $states = State::all();
 
@@ -32,11 +32,11 @@ class TravelRequestController extends Controller
 
         $transports = TravelTransport::all();
 
-
-        $travel_requests = TravelRequest::orderBy('TravelRef', 'DESC')->get()
-                                                                      ->Where('SentForApproval', '0')
-                                                                      ->Where('RequesterID', Auth::user());
-        $lodges = TravelLodge::all();
+        $travel_requests = TravelRequest::orderBy('TravelRef', 'DESC')
+            ->Where('SentForApproval', '0')
+            ->Where('RequesterID', Auth::user()->id)
+            ->get();
+        $lodges      = TravelLodge::all();
         $travelmodes = TravelMode::all();
 
         $user = User::all();
@@ -57,31 +57,28 @@ class TravelRequestController extends Controller
             $filenametostore       = $filename . '_' . time() . '.' . $extension;
             $request->file('ReferenceLetter')->storeAs('public/reference_letter', $filenametostore);
 
-            $travel_request                = new TravelRequest;
+            $travel_request                  = new TravelRequest;
             $travel_request->ReferenceLetter = $filenametostore;
-            $travel_request->entered_by = $user_id;
+            $travel_request->entered_by      = $user_id;
             $travel_request->save();
         }
 
-        if($travel_request->save()){
+        if ($travel_request->save()) {
             return redirect()->route('travel_request.create')->with('success', 'Request was added successfully');
         }
     }
 
-
-    //Edit travel request function 
+    //Edit travel request function
     public function edit_travel_request($id)
     {
-        $travel_request  = TravelRequest::find($id);
+        $travel_request = TravelRequest::find($id);
         return response()->json($travel_request);
     }
 
-
-    //submit update travel request function 
+    //submit update travel request function
     public function submit_travel_request(Request $request)
     {
         $travel_request = TravelRequest::find($request->TravelRef);
-
 
         if ($request->hasFile('ReferenceLetter')) {
             $filenamewithextension = $request->file('ReferenceLetter')->getClientOriginalName();
@@ -90,13 +87,12 @@ class TravelRequestController extends Controller
             $filenametostore       = $filename . '_' . time() . '.' . $extension;
             $request->file('ReferenceLetter')->storeAs('public/reference_letter', $filenametostore);
             $travel_request->ReferenceLetter = $filenametostore;
-            $travel_request->entered_by = $user_id;
+            $travel_request->entered_by      = $user_id;
         }
         $travel_request->update($request->except(['_token']));
 
         return redirect()->route('travel_request.create')->with('success', 'Request was updated successfully');
     }
-
 
     //Delete travel request function
     public function destroy($id)
@@ -104,15 +100,15 @@ class TravelRequestController extends Controller
         $travel_request = TravelRequest::find($id);
 
         $travel_request->delete();
-        
+
         return redirect()->route('travel_request.create')->with('success', 'Request Deleted successfully');
     }
 
     //Approval function
     public function send_for_approval(Request $request, $ref)
     {
-        
-        $states = State::all();
+
+        $states    = State::all();
         $countries = Country::all();
 
         $staffs = Staff::all();
@@ -121,23 +117,23 @@ class TravelRequestController extends Controller
 
         $travel_requests = TravelRequest::orderBy('TravelRef', 'DESC')->get();
 
-        $lodges = TravelLodge::all();
+        $lodges      = TravelLodge::all();
         $travelmodes = TravelMode::all();
 
         $user = User::all();
 
         $travel_request = User::all();
         $travel_request = TravelRequest::where('TravelRef', $ref)
-                                        ->where('SentForApproval', '0')
-                                        ->first();
+            ->where('SentForApproval', '0')
+            ->first();
         $travel_request->SentForApproval = '1';
         $travel_request->update();
-        
+
         $email = Staff::find($travel_request->Approver)->first()->user->email;
 
         Mail::to($email)->send(new SendforApproval());
 
-        return redirect()->route('travel_request.create', compact('states','countries', 'staffs', 'travel_requests', 'transports', 'lodges', 'travelmodes'))->with('success', 'Request was Sent successfully');
+        return redirect()->route('travel_request.create', compact('states', 'countries', 'staffs', 'travel_requests', 'transports', 'lodges', 'travelmodes'))->with('success', 'Request was Sent successfully');
 
     }
 
@@ -151,36 +147,32 @@ class TravelRequestController extends Controller
 
         $user = User::all();
 
-
-
         return view('travel_request.admindashboard', compact('travel_requests'));
     }
 
-
     //Approve travel request function
     public function approve_request(Request $request, $ref)
-    {   
+    {
         $staffs = Staff::all();
 
         $user = User::all();
 
         $travel_request = User::all();
 
-        $travel_request = TravelRequest::find($ref);
+        $travel_request               = TravelRequest::find($ref);
         $travel_request->ApprovalDate = date('Y-m-d');
-        $travel_request = TravelRequest::where('TravelRef', $ref)
-                                        ->get()
-                                         ->first();
-                                        //  dd($travel_request);
+        $travel_request               = TravelRequest::where('TravelRef', $ref)
+            ->get()
+            ->first();
+        //  dd($travel_request);
 
         $email = Staff::find($travel_request->RequesterID)->first()->user->email;
         // dd($email);
 
         Mail::to($email)->send(new RequestApproved());
-                                         
+
         return redirect()->route('travel_request.admindashboard')->with('success', 'Request Approved successfully');
     }
-
 
     //Reject travel request function
     public function reject_request(Request $request, $ref)
@@ -191,7 +183,7 @@ class TravelRequestController extends Controller
 
         $travel_request = User::all();
 
-        $travel_request = TravelRequest::find($ref);
+        $travel_request                  = TravelRequest::find($ref);
         $travel_request->SentForApproval = 0;
         $travel_request->save();
 
@@ -201,4 +193,3 @@ class TravelRequestController extends Controller
         return redirect()->route('travel_request.admindashboard')->with('success', 'Request Rejected successfully');
     }
 }
-
