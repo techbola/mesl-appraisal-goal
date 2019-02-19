853e24874abb40c58a9f0d389462371323367d20
Riliwan Balogun <riliwan.rabo@gmail.com>
2019-02-19 17:40:43 +0100

    Leave requests in tabs

--- resources/views/leave_request/index.blade.php
+++ resources/views/leave_request/index.blade.php
@@ -49,20 +49,28 @@
   				</div>
   			</div>
   			<div class="clearfix"></div>
-  			
-        <table class="table tableWithSearch table-bordered">
-          <thead>
-            <th >Leave Type</th>
-            <th >Start Date</th>
-            <th >End Date</th>
-            <th >Leave Days</th>
-            <th>Files(s)</th>
-            <th >Awaiting Approval</th>
-            <th >Action</th>
-          </thead>
-          <tbody>
 
-            @foreach($leave_requests as $leave_request)
+        <ul class="nav nav-tabs outside">
+        <li class="active"><a data-toggle="tab" href="#unapproved">Pending Requests &nbsp; <span class="badge badge-warning">{{ $leave_requests->where('CompletedFlag', 0)->count() }}</span></a></li>
+        <li><a data-toggle="tab" href="#approved">Approved Requests &nbsp; <span class="badge badge-success">{{ $leave_requests->where('CompletedFlag', 1)->count() }}</span></a></li>
+      </ul>
+      <div class="tab-content">
+        <div id="unapproved" class="tab-pane fade in active">
+          
+            <div class="card-box ">
+                <table class="table tableWithSearch">
+                  <thead>
+                   <th >Leave Type</th>
+                    <th >Start Date</th>
+                    <th >End Date</th>
+                    <th >Leave Days</th>
+                    <th>Files(s)</th>
+                    <th >Awaiting Approval</th>
+                    <th >Action</th>
+
+                  </thead>
+                  <tbody>
+                    @foreach($leave_requests->where('CompletedFlag', 0) as $leave_request)
                 <tr>
                 <td>{{$leave_request->leave_type->LeaveType}}</td>
                 <td>{{$leave_request->StartDate}}</td>
@@ -109,9 +117,81 @@
                 </td>
               </tr>
               @endforeach
-          </tbody>
-        </table> 
+                  </tbody>
+                </table>
+            </div>
+        </div>
 
+        <div id="approved" class="tab-pane fade">
+
+          
+          <div class="card-box">
+            <table class="table tableWithSearch">
+              <thead>
+               <th >Leave Type</th>
+            <th >Start Date</th>
+            <th >End Date</th>
+            <th >Leave Days</th>
+            <th>Files(s)</th>
+            <th >Awaiting Approval</th>
+            <th >Action</th>
+              </thead>
+              <tbody>
+               @foreach($leave_requests->where('CompletedFlag', 1) as $leave_request)
+                <tr>
+                <td>{{$leave_request->leave_type->LeaveType}}</td>
+                <td>{{$leave_request->StartDate}}</td>
+                <td>{{$leave_request->ReturnDate}}</td>
+                <td>{{$leave_request->NumberofDays}}</td>
+                <td>
+                  @if(!is_null($leave_request->HandOverNote))
+                    <a href="{{ asset( 'storage/leave_document/'.$leave_request->HandOverNote)}}" class="btn btn-xs btn-success" target="_blank">
+                  Download attachment
+                  @else
+                    <span class="badge">No Files Uploaded</span>
+                  @endif
+                </td>
+                <td>
+                  @if($leave_request->NotifyFlag == 1 && $leave_request->RejectionFlag != 1 && is_null($leave_request->ApproverID) && $leave_request->CompletedFlag == 1 )
+                  <label class="label label-success"> Your Leave Request Awaits HR Approval. </label>
+                  @else
+                    @if($leave_request->NotifyFlag == 0 && $leave_request->RejectionFlag != 1)
+                    <label class="label label-default">Leave Request has not been sent for approval.</label>
+                  @elseif($leave_request->NotifyFlag == 1 && $leave_request->RejectionFlag != 1)
+                    @if($leave_request->SupervisorApproved != 1)
+                    <label class="label label-default"> pending with {{$leave_request->staff->user->first_name ?? ''}} {{$leave_request->user->last_name ?? ''}}</label>
+                    @elseif($leave_request->ApproverID == 0 && $leave_request->CompletedFlag == 1)
+                    <label class="label label-success">completed</label>
+                    @elseif($leave_request->ApproverID == 0 && $leave_request->CompletedFlag == 0)
+                    <label class="label label-default"> pending with HR</label>
+                    @else
+                    <label class="label label-default"> pending with {{$leave_request->current_approver->FullName ?? ''}} </label>
+                    @endif
+                  @else
+                  <label class="label label-danger"> Rejected by {{$leave_request->staff->user->first_name ?? ''}} {{$leave_request->staff->user->last_name ?? ''}}</label>
+                  @endif
+                  @endif
+                </td>
+                <td>
+                  @if($leave_request->NotifyFlag == 0)
+                  <a href="/leave_request/{{ $leave_request->LeaveReqRef }}/edit" class="btn btn-xs btn-complete " data-request_ref = "{{ $leave_request->LeaveReqRef }}" >Edit Request</a> 
+                  | <a href="#" class="btn btn-xs btn-success" data-id="{{$leave_request->LeaveReqRef}}" onclick="send_notification()">Send for Approval</a>
+                  @elseif($leave_request->NotifyFlag == 1 && $leave_request->RejectionFlag != 1 && is_null($leave_request->ApproverID) && $leave_request->CompletedFlag == 1)
+                    <p><i style="font-size: 12px; color: green">Completed</i></p>
+                  @else
+                    <p><i style="font-size: 12px; color: green">Request can't be edited again</i></p>
+                  @endif 
+                </td>
+              </tr>
+              @endforeach
+              </tbody>
+            </table>
+          </div>
+
+        </div>
+      </div>  
+  			
+       
   	</div>
   	<!-- END PANEL -->
 @endsection
