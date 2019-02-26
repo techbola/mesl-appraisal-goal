d4aa5d7818823e8f34e0596127c0bbc6749d9f81
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2019-02-26 17:47:57 +0100

    update Onboarding fixes

--- resources/views/staff/onboard_dashboard_admin.blade.php
+++ resources/views/staff/onboard_dashboard_admin.blade.php
@@ -16,69 +16,134 @@
 @endpush
 
 @section('content')
-<div class="card-box">
-        <div class="card-title pull-left">Staff Onboarding Requests</div>
-        <div class="pull-right">
-            <div class="col-xs-12">
-                <input type="text" class="search-table form-control pull-right" placeholder="Search">
-            </div>
+  <div class="card-box">
+    <div class="card-title pull-left">Staff Onboarding Requests</div>
+    <div class="pull-right">
+      <div class="col-xs-12">
+        <input type="text" class="search-table form-control pull-right" placeholder="Search">
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="">
+      <ul class="nav nav-tabs outside">
+        <li class="active">
+          <a data-toggle="tab" href="#unresolved">
+            Pending &nbsp; <span class="badge badge-warning"></span>
+          </a>
+        </li>
+        <li>
+          <a data-toggle="tab" href="#resolved">
+            Completed &nbsp; <span class="badge badge-success"></span>
+          </a>
+        </li>
+      </ul>
+      <div class="tab-content">
+        <div id="unresolved" class="tab-pane fade in active">
+          <table class="table tableWithSearch table-bordered">
+            <thead>
+              <th width="10%">Employee</th>
+              <th width="7%">Department</th>
+              <th width="7%">Employment</th>
+              <th width="5%">Resumption</th>
+              <th width="12%">Office Assets</th>
+              <th width="15%">Action</th>
+            </thead>
+            <tbody>
+              @foreach($staff_onboards as $staff_onboard)
+                @if($staff_onboard->ApprovalStatus2 !== "1")  
+                  <tr>
+                    <td>{{$staff_onboard->StaffName}}</td>
+                      <td>{{$staff_onboard->staff_department->name}}</td>
+                      <td>{{$staff_onboard->StaffType}}</td>
+                      <td>{{$staff_onboard->ResumptionDate}}</td>
+                      <td>
+                        @if(!empty($staff_onboard->OfficeSpace) && empty($staff_onboard->OfficeTable))
+                          @php
+                            $staff_onboard->OfficeSpace = $staff_onboard->OfficeSpace
+                          @endphp
+                        @elseif(!empty($staff_onboard->OfficeSpace))
+                          @php
+                            $staff_onboard->OfficeSpace = $staff_onboard->OfficeSpace.", "
+                          @endphp
+                        @endif
+                        @if(!empty($staff_onboard->OfficeTable) && !empty($staff_onboard->BusinessCard))
+                          @php
+                            $staff_onboard->OfficeTable = $staff_onboard->OfficeTable.", "
+                          @endphp
+                        @endif
+                        {{$staff_onboard->OfficeSpace}} {{$staff_onboard->OfficeTable}} {{$staff_onboard->BusinessCard}}
+                      </td>
+                    <td>
+                        <a href="{{ url('admin_onboard_mail')}} / {{$staff_onboard->StaffOnboardRef }}" class="btn btn-xs btn-success">
+                          <i class="fa fa-share-square"></i> Mark as Done
+                        </a>
+                    </td>
+                  </tr>
+                @endif
+                @endforeach
+            </tbody>
+          </table> 
         </div>
-        <div class="clearfix"></div>
-            
-      <table class="table tableWithSearch table-bordered">
-        <thead>
-          <th width="10%">Employee</th>
-          <th width="7%">Department</th>
-          <th width="7%">Employment</th>
-          <th width="5%">Resumption</th>
-          <th width="12%">Office Assets</th>
-          <th width="15%">Action</th>
-        </thead>
-        <tbody>
-
-          @foreach($staff_onboards as $staff_onboard)
-            @if($staff_onboard->ApprovalStatus2 !== "1")  
-              <tr>
-                <td>{{$staff_onboard->StaffName}}</td>
-                  <td>{{$staff_onboard->staff_department->name}}</td>
-                  <td>{{$staff_onboard->StaffType}}</td>
-                  <td>{{$staff_onboard->ResumptionDate}}</td>
-                  <td>
-                    {{$staff_onboard->OfficeSpace}} {{$staff_onboard->OfficeTable}} {{$staff_onboard->BusinessCard}}
-                  </td>
-                <td>
-                    <a href="{{ url('admin_onboard_mail')}}/ {{$staff_onboard->StaffOnboardRef }}" class="btn btn-xs btn-success">
-                      <i class="fa fa-share-square"></i> Mark as Done
-                    </a>
-                </td>
-              </tr>
-            @endif
-            @endforeach
-        </tbody>
-      </table> 
-</div>
-
-
-
-
-
+        <div id="resolved" class="tab-pane fade">        
+          <table class="table tableWithSearch table-bordered">
+            <thead>
+              <th width="10%">Employee</th>
+              <th width="7%">Department</th>
+              <th width="7%">Employment</th>
+              <th width="5%">Resumption</th>
+              <th width="12%">Office Assets</th>
+              <th width="15%">Action</th>
+            </thead>
+            <tbody>
+              @foreach($staff_onboards as $staff_onboard)
+                @if($staff_onboard->ApprovalStatus2 !== "0")  
+                  <tr>
+                    <td>{{$staff_onboard->StaffName}}</td>
+                      <td>{{$staff_onboard->staff_department->name}}</td>
+                      <td>{{$staff_onboard->StaffType}}</td>
+                      <td>{{$staff_onboard->ResumptionDate}}</td>
+                      <td>
+                        @if(!empty($staff_onboard->OfficeSpace) && empty($staff_onboard->OfficeTable))
+                          @php
+                            $staff_onboard->OfficeSpace = $staff_onboard->OfficeSpace
+                          @endphp
+                        @elseif(!empty($staff_onboard->OfficeSpace))
+                          @php
+                            $staff_onboard->OfficeSpace = $staff_onboard->OfficeSpace.", "
+                          @endphp
+                        @endif
+                        @if(!empty($staff_onboard->OfficeTable) && !empty($staff_onboard->BusinessCard))
+                          @php
+                            $staff_onboard->OfficeTable = $staff_onboard->OfficeTable.", "
+                          @endphp
+                        @endif
+                        {{$staff_onboard->OfficeSpace}} {{$staff_onboard->OfficeTable}} {{$staff_onboard->BusinessCard}}
+                      </td>
+                    <td>
+                      <button class="btn btn-success btn-xs">
+                        <i class="fa fa-check"></i> Done
+                      </button>
+                    </td>
+                  </tr>
+                @endif
+                @endforeach
+            </tbody>
+          </table> 
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
-
 @push('scripts')
-<script src="{{ asset('assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js') }}" type="text/javascript">
-</script>
-
-<script>
-    
-
-     $(document).ready(function() {
-    $('#travelTable').DataTable( {
+  <script src="{{ asset('assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js') }}" type="text/javascript"></script>
+  <script>
+    $(document).ready(function() {
+      $('#travelTable').DataTable({
         "scrollX": true
-    } );
-} );
-
-
-</script>
-
+      });
+    });
+  </script>
 @endpush
