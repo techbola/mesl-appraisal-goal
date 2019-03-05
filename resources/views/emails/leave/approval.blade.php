32cb76dd4eaea1bafc222b6670d2f7703ea9a30f
Riliwanrabo <rabo.sandbox@gmail.com>
2019-03-05 11:02:20 +0100

    Leave Request Email Templates

--- resources/views/emails/leave/approval.blade.php
+++ resources/views/emails/leave/approval.blade.php
@@ -3,8 +3,10 @@ Title : Leave Request Approval.
 
 Dear {{$leave_request->current_approver->FullName}}
 
-{{ $leave_request->requester->fullName }} Requested a  ({{ $leave_request->NumberofDays }}) day(s) <b>{{ $leave_request->leave_type->LeaveType }} Leave</b>. <br>
+{{ $leave_request->requester->fullName }} requested a  ({{ $leave_request->NumberofDays }}) day(s) <b>{{ $leave_request->leave_type->LeaveType }} Leave</b>. <br>
+
 Start Date: <b>{{ nice_date($leave_request->StartDate) }}</b> <br>
+
 End Date: <b>{{ nice_date($leave_request->ReturnDate) }}</b> 
 
 You are required to approve or decline their request.
