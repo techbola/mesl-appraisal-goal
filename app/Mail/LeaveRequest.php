be4bd00dcf4623d6d7f04527c29fbc518c924fc8
Dipo Odeyemi <dipo.odeyemi@cavidel.com>
2019-03-28 11:41:27 +0100

    leave approval

--- app/Mail/LeaveRequest.php
+++ app/Mail/LeaveRequest.php
@@ -5,33 +5,29 @@ namespace MESL\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
-class LeaveRequest extends Mailable
-{
-    use Queueable, SerializesModels;
+class LeaveRequest extends Mailable {
+	use Queueable, SerializesModels;
 
-    public $name;
-    public $leave_request;
+	public $name;
+	public $leave_request;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($name, $leave_request)
-    {
-        $this->name          = $name;
-        $this->leave_request = $leave_request;
-    }
+	/**
+	 * Create a new message instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($name, $leave_request) {
+		$this->name = $name;
+		$this->leave_request = $leave_request;
+	}
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this->markdown('emails.LeaveRequest');
-    }
+	/**
+	 * Build the message.
+	 *
+	 * @return $this
+	 */
+	public function build() {
+		return $this->markdown('emails.LeaveRequest');
+	}
 }
